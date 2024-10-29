cd799c2927ad5ef1f6b0d5e98776e844c7265c6f
nsierra <nicofsierra@gmail.com>
2024-10-29 17:33:03 -0300

    navegacion entre activities

--- app/src/main/java/com/example/tp_2_m3_2c_2024/FuncActivity.java
+++ app/src/main/java/com/example/tp_2_m3_2c_2024/FuncActivity.java
@@ -1,9 +1,15 @@
 package com.example.tp_2_m3_2c_2024;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,10 +20,38 @@ public class FuncActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_func);
+        Toolbar toolbar = findViewById(R.id.mytoolbar);
+        setSupportActionBar(toolbar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.func), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.activity1: startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.activity2: startActivity(new Intent(this, FuncActivity.class));
+                break;
+            default:
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/tp_2_m3_2c_2024/MainActivity.java
+++ app/src/main/java/com/example/tp_2_m3_2c_2024/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.tp_2_m3_2c_2024;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.mytoolbar);
+
         setSupportActionBar(toolbar);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -29,9 +34,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu,menu);
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.activity1: startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.activity2: startActivity(new Intent(this, FuncActivity.class));
+                break;
+            default:
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
